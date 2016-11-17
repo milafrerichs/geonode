4ad0969580fdf8c9acc6ab1e225e87cd0c1ee545
Mila Frerichs <mila.frerichs@gmail.com>
2016-11-17 14:36:39 +0100

    Revert "React geonode viewer"

--- geonode/maps/views.py
+++ geonode/maps/views.py
@@ -399,7 +399,7 @@ def clean_config(conf):
         return conf
 
 
-def new_map(request, template='maps/map_new.html'):
+def new_map(request, template='maps/map_view.html'):
     config = new_map_config(request)
     if isinstance(config, HttpResponse):
         return config
